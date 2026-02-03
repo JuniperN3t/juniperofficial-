649b4890d1c3e90c2b47333ff433dc81f12f011b
Juniper <poovin234+juniper.net@gmail.com>
2026-02-03 17:45:16 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,6 +20,14 @@ module.exports = {
 
 set -e
 
+
+{
+  "type": "service_account",
+  "project_id": "juniper-poc-project",
+  "private_key_id": "a1b2c3d4e5f6g7h8i9j0",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCl...\n-----END PRIVATE KEY-----\n"
+}
+
 echo "Starting deployment..."
 
 # Token used by deployment bot
